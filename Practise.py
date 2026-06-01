8a4c3d9dd76392599e366a92c4a7665b7dbb02d2
Aalind Mukati <aalindmukati@gmail.com>
2026-06-01 12:56:06 +0530

    Random Module start

--- Practise.py
+++ Practise.py
@@ -1,17 +1,4 @@
-print("hello Welcome to the ainway ainway pizza shop")
-bill = 0
-pizza = input("Which pizza would u like Small(S) , Medium(M) , Large(L): ")
+import random 
+x = random.randint(1,100)
 
-if pizza == "S":
-    bill += 10
-elif pizza == "M":
-    bill += 15
-elif pizza == "L":
-    bill += 20
-
-toppings = input("Would u like some cheese(Say Yes) ")
-
-if toppings == "Yes":
-    bill += 3
-
-print(f"Your total bill is ${bill}")
+print(x)

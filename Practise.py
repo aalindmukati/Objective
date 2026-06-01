cc45599c9b979d21235ed9646be355f1908840dc
Aalind Mukati <aalindmukati@gmail.com>
2026-06-01 22:48:12 +0530

    For mastering

--- Practise.py
+++ Practise.py
@@ -1,12 +1,15 @@
 random_numbers = [263, 128, 106, 289, 170, 162, 157, 135, 288, 126, 273, 289, 239, 122, 251, 208, 108, 107, 123, 155]
 
-max_score = 0
+#max_score = 0
+
+#for score in random_numbers:
+#    if score>max_score:
+#        max_score=score
+
+#print(max_score)
+total_sum = 0
 
 for score in random_numbers:
-    if score>max_score:
-        max_score=score
+    total_sum = total_sum+score
 
-print(max_score)
-max_score = max(random_numbers)
-
-print(max_score)
+    print(total_sum)

b853cc2adb41e9cad7a98de586bdbc6dc78af92f
Aalind Mukati <aalindmukati@gmail.com>
2026-06-01 22:26:53 +0530

    max score for learnt

--- Practise.py
+++ Practise.py
@@ -1,4 +1,12 @@
-import random 
+random_numbers = [263, 128, 106, 289, 170, 162, 157, 135, 288, 126, 273, 289, 239, 122, 251, 208, 108, 107, 123, 155]
 
-x = ["idk","adi"]
-print(random.choice(x))
+max_score = 0
+
+for score in random_numbers:
+    if score>max_score:
+        max_score=score
+
+print(max_score)
+max_score = max(random_numbers)
+
+print(max_score)

f7ff3c199528902a97ea5de72b4a78a8d86e62c7
Aalind Mukati <aalindmukati@gmail.com>
2026-06-01 12:03:13 +0530

    The Second commt

--- Practise.py
+++ Practise.py
@@ -1,1 +1,12 @@
-print("hello")
+print("hello Welcome to the ainway ainway pizza shop")
+bill = 0
+pizza = input("Which pizza would u like Small(S) , Medium(M) , Large(L): ")
+
+if pizza == "S":
+    bill += 10
+elif pizza == "M":
+    bill += 15
+elif pizza == "L":
+    bill += 20
+
+print(f"Your total bill is ${bill}")

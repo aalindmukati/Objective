f4bb7c5dadde9b835f2e1d21fa8223444b2627f1
Aalind Mukati <aalindmukati@gmail.com>
2026-06-02 17:41:34 +0530

    Update

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,14 @@
+import random
+
+tries = 0
+
 word_list = [
     "python", "galaxy", "wizard", "quartz", "avalanche", 
     "blizzard", "matrix", "oxygen", "voodoo", "unknown", 
     "jiujitsu", "luxury", "subway", "swivel", "avenue", 
     "gossip", "icebox", "microwave", "jackpot", "khaki"
-]
+] 
+gamed_name = random.choice(word_list)
+print(word_list)
+
+

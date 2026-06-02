69483bb6d172d2a7deb3c41826232ead21020484
Aalind Mukati <aalindmukati@gmail.com>
2026-06-02 17:55:36 +0530

    Final Upgrade done

--- Hangman.py
+++ Hangman.py
@@ -1,14 +1,26 @@
 import random
 
 tries = 0
-
 word_list = [
     "python", "galaxy", "wizard", "quartz", "avalanche", 
     "blizzard", "matrix", "oxygen", "voodoo", "unknown", 
     "jiujitsu", "luxury", "subway", "swivel", "avenue", 
     "gossip", "icebox", "microwave", "jackpot", "khaki"
-] 
+]
+
 gamed_name = random.choice(word_list)
-print(word_list)
+print("Word list:", word_list)
+print("-------------------------------------")
+print("Guess a word from the word list")
 
-
+while True:
+    guess = input("Guess: ").lower()
+    print("-------------------------------------")
+    tries += 1
+    
+    if guess == gamed_name:
+        print(f'Wow! You guessed the correct word, which was "{gamed_name}"!')
+        print(f'It took you {tries} tries.')
+        break
+    else:
+        print("Incorrect! Try again.")

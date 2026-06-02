b7e5a6cf2494830d6805319db76eb5043fc59d47
Aalind Mukati <aalindmukati@gmail.com>
2026-06-02 17:27:19 +0530

    Hangman Game

---
+++ Hangman.py
@@ -0,0 +1,6 @@
+word_list = [
+    "python", "galaxy", "wizard", "quartz", "avalanche", 
+    "blizzard", "matrix", "oxygen", "voodoo", "unknown", 
+    "jiujitsu", "luxury", "subway", "swivel", "avenue", 
+    "gossip", "icebox", "microwave", "jackpot", "khaki"
+]
--- Practise.py
+++ Practise.py
@@ -1,15 +1,5 @@
-random_numbers = [263, 128, 106, 289, 170, 162, 157, 135, 288, 126, 273, 289, 239, 122, 251, 208, 108, 107, 123, 155]
+def my_function():
+    print("What did u do")
+    print("Watch ur tone boy")
 
-#max_score = 0
-
-#for score in random_numbers:
-#    if score>max_score:
-#        max_score=score
-
-#print(max_score)
-total_sum = 0
-
-for score in random_numbers:
-    total_sum = total_sum+score
-
-    print(total_sum)
+my_function()

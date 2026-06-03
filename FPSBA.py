377836689e02bb2c71812aca98fd8780ad88944b
Aalind Mukati <aalindmukati@gmail.com>
2026-06-03 19:42:20 +0530

    Getting started

--- FPSBA.py
+++ FPSBA.py
@@ -1,1 +1,8 @@
 print("First price Based auction")
+print('''-------------------------------------------------------------------------------------------------------------------------------------------------''')
+
+Player = []
+name = input('What is ur first name ')
+Player.append(name)
+Bid = int(input('How much money u wanna bid '))
+

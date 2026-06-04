f18cbdd9a43cb6e5d78b53edc101d5551870c588
Aalind Mukati <aalindmukati@gmail.com>
2026-06-04 12:42:14 +0530

    Setting a skeletal struct of pgm

--- BlackJack.py
+++ BlackJack.py
@@ -6,11 +6,22 @@ print("---------------------")
 blackjack_values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10', '10', '10', 'A']
 
 User_card=[]
+Comp_card =[]
+
+for _ in range(2):
+    pata = random.choice(blackjack_values)
+    User_card.append(pata)
+    print(f'the User card is {User_card}')
+
+for _ in range(2):
+    pata = random.choice(blackjack_values)
+    Comp_card.append(pata)
+    print(f'the Computer card is {Comp_card}')
 
 choice = input("Would u like to hit or stand ").lower()
 if choice == 'hit':
     pata = random.choice(blackjack_values)
     User_card.append(pata)
-
-
-print(User_card)
+    print(User_card)
+else:
+    print("ok")

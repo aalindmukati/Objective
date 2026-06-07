a71af82abcd18c8467cac24713b0a593fe5fbe4e
Aalind Mukati <aalindmukati@gmail.com>
2026-06-07 11:57:25 +0530

    Understanding

--- Class.py
+++ Class.py
@@ -1,2 +1,14 @@
 class User:
-    
+    def __init__(self):
+        print("New user blah blah")
+
+
+user_1 = User()
+user_1.id = "1"
+user_1.name = 'aalind'
+
+print(user_1.id,user_1.name)
+
+user_2 = User()
+user_2.id = "2"
+user_2.name = 'Kratos'

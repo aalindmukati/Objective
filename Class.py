d527467bfa58e1b6583230f3c22bc6db923d9564
Aalind Mukati <aalindmukati@gmail.com>
2026-06-07 17:34:24 +0530

    Methods for a class

--- Class.py
+++ Class.py
@@ -2,6 +2,9 @@ class User:
     def __init__(self, user_id , name):
         self.id = user_id
         self.name = name
+        self.follower = 0
+
+    def follow(self):
 
 
 user_1 = ('1',"aalind")

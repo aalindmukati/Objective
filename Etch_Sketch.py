2848b840f05f1608958d1f004f0d76e04f0d6c20
Aalind Mukati <aalindmukati@gmail.com>
2026-06-08 18:05:00 +0530

    Etch sketch complete ig

--- Etch_Sketch.py
+++ Etch_Sketch.py
@@ -6,9 +6,15 @@ screen = Screen()
  
 def forward():
     Aj.forward(20)
-
+def left():
+    Aj.left(90)
+def right():
+    Aj.right(90)
 
 screen.listen()
 screen.onkey(key ="space" , fun=forward)
+screen.onkey(key="d", fun=left)
+screen.onkey(key="a", fun=right)
+
 
 screen.exitonclick()

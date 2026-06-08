eb786e415576e3167acaee0cc41f5444f9675f05
Aalind Mukati <aalindmukati@gmail.com>
2026-06-08 17:21:32 +0530

    s with rabdom dirn

--- Spectrogram.py
+++ Spectrogram.py
@@ -4,16 +4,24 @@ import random
 Aj = Turtle()
 screen = Screen()
 
-colors = ['red','blue','green','cyan','black',]
-size = [0,30,45,60,90,120,270,180]
+screen.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    color = (r,g,b)
+    return color
+
+angle = [0,30,45,60,90,120,270,180]
 
 screen.screensize(100,100)
 Aj.speed(30)
 
-for path in range(25):
-    Aj.color(random.choice(colors))
-    Aj.circle(15)
-    Aj.left(15)
+for path in range(50):
+    Aj.color(random_color())
+    Aj.circle(30)
+    Aj.left(random.choice(angle))
 
 
 screen.exitonclick()

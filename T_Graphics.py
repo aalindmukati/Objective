dbc4e3aa32a5b88613c36a52e08e0b16b2cfb6e7
Aalind Mukati <aalindmukati@gmail.com>
2026-06-08 11:02:10 +0530

    Revamping some things

--- T_Graphics.py
+++ T_Graphics.py
@@ -3,4 +3,10 @@ from turtle import Turtle,Screen
 jason = Turtle()
 my_screen = Screen()
 
+jason.shape("turtle")
+jason.color('green')
+my_screen.bgcolor('orange')
+
+jason.forward(100)
+
 my_screen.exitonclick()

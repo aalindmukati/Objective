4e6718bc2ef5b2c9519e6991008cc2304e5b2404
Aalind Mukati <aalindmukati@gmail.com>
2026-06-08 10:04:00 +0530

    Fixed indent under Class

--- Class.py
+++ Class.py
@@ -1,20 +1,19 @@
 class User:
-    def __init__(self, user_id , name , follower , following):
+    def __init__(self, user_id , name):
         self.id = user_id
         self.name = name
         self.follower = 0
-        self.following
+        self.following = 0
 
     def follow(self, user):
         user.follower += 1
         self.following += 1
 
-user_1 = ('1',"aalind")
-user_2 = ("2","Kratos")
+user_1 = User('1',"aalind")
+user_2 = User("2","Kratos")
 
-user_2.follow(user_1)
 
-print(user_1,user_2)
+user_1.follow(user_2)
 print(user_1.follower)
 print(user_1.following)
 print(user_2.follower)

ee9c734a26fc50b20a09a7905bbba2d56bbc303c
Aalind Mukati <aalindmukati@gmail.com>
2026-06-09 12:07:27 +0530

    Added the if,while to move forward

--- Horse.py
+++ Horse.py
@@ -1,21 +1,32 @@
 from turtle import Turtle,Screen
 import random
 
-
+is_race_on = False
 screen = Screen()
 color=['red','blue','green','purple','black','orange']
 screen.setup(500,400)
 user_bet =screen.textinput('make ur bet', 'which object will win the race  red, blue, green, purple, black or orange')
 print(user_bet)
-y = [10,40,70,100,120]
+y = [10,40,70,100,130,160]
+all_racers = []
+
 
 for turtle_thing in range(0,6):
     Aj = Turtle('turtle')
     Aj.color(color[turtle_thing])
     Aj.penup()
     Aj.goto(-235,y=y[turtle_thing])
+    all_racers.append(Aj)
 
+if user_bet:
+    is_race_on=True
 
+while is_race_on:
 
+    for racers in all_racers:
+        randi_dist = random.randint(0,10)
+        racers.forward(randi_dist)
+    
 
 screen.exitonclick()
+

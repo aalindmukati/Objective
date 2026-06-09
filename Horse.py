567b3bedb7e5d5fe81229ba7d4599413bd44535c
Aalind Mukati <aalindmukati@gmail.com>
2026-06-09 11:52:32 +0530

    Fixed Some things

--- Horse.py
+++ Horse.py
@@ -3,17 +3,19 @@ import random
 
 
 screen = Screen()
-color=['red','blue','green','purple','black']
+color=['red','blue','green','purple','black','orange']
 screen.setup(500,400)
-user_bet =screen.textinput('make ur bet', 'which object will win the race')
+user_bet =screen.textinput('make ur bet', 'which object will win the race  red, blue, green, purple, black or orange')
 print(user_bet)
-y = ['10','30','50','70','90']
+y = [10,40,70,100,120]
 
 for turtle_thing in range(0,6):
     Aj = Turtle('turtle')
-    Aj.color(color=color[turtle_thing])
+    Aj.color(color[turtle_thing])
     Aj.penup()
     Aj.goto(-235,y=y[turtle_thing])
+
+
 
 
 screen.exitonclick()

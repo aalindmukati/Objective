dd4b7433c786eae4c60a13745521aba568cac87f
Aalind Mukati <aalindmukati@gmail.com>
2026-06-09 12:40:58 +0530

    Movement Skeletal structure

--- Snake_Game.py
+++ Snake_Game.py
@@ -1,4 +1,21 @@
 from turtle import Turtle,Screen
 
+Aj = Turtle()
 screen = Screen()
 screen.setup(600,600)
+screen.bgcolor('purple')
+
+def forward():
+    Aj.forward(20)
+def left():
+    Aj.left(90)
+def right():
+    Aj.right(90)
+
+screen.listen()
+screen.onkey(key ="space" , fun=forward)
+screen.onkey(key="d", fun=left)
+screen.onkey(key="a", fun=right)
+
+
+screen.exitonclick()

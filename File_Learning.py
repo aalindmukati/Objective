ac6f815d0f55af65000e4b794359edb7646d3ef7
Aalind Mukati <aalindmukati@gmail.com>
2026-06-11 18:25:50 +0530

    Completed file reading

--- File_Learning.py
+++ File_Learning.py
@@ -1,3 +1,10 @@
 with open("names.txt") as names_file:
-    name = names_file.read()
-    print(name)
+    name = names_file.read().splitlines()
+
+PLACEHOLDER = '[name]'
+
+with open("file.txt") as letter:
+    letter_content = letter.read()
+    for name in name:
+        updated_letter = letter_content.replace(PLACEHOLDER, name)
+        print(updated_letter)

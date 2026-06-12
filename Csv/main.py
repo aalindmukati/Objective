a6f5a603e080a3231f3bf7eaf1e59e4d7def034e
Aalind Mukati <aalindmukati@gmail.com>
2026-06-12 12:18:17 +0530

    Learning individual printing

--- Csv/main.py
+++ Csv/main.py
@@ -1,2 +1,14 @@
-with open("weather_data.csv") as data_file:
-    data = data_file.read()
+# with open("Csv/weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+import csv
+
+
+with open("Csv/weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temp = []
+    for row in data:
+        if row[0] != 'day':
+            temp.append(row[0])
+            print(row[0])

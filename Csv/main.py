0cad41deb40e97c84ed3690de3926e17a3337c89
Aalind Mukati <aalindmukati@gmail.com>
2026-06-12 13:03:05 +0530

    Installing pandas

--- Csv/main.py
+++ Csv/main.py
@@ -2,13 +2,17 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
+# import csv
 
 
-with open("Csv/weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temp = []
-    for row in data:
-        if row[0] != 'day':
-            temp.append(row[0])
-            print(row[0])
+# with open("Csv/weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temp = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temp.append(row[1])
+            
+# print(f'So the temperatures are {temp}')
+
+import pandas as pd 
+pd.read_csv("Csv/weather_data.csv")

2763833e270832745b0ef73e83640c6b533b566e
Aalind Mukati <aalindmukati@gmail.com>
2026-06-13 16:59:48 +0530

    Creating Nato pnemonics scheme

--- Practise.py
+++ Practise.py
@@ -1,9 +1,27 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-shift = 3
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
 
-# Shift right by N positions
-shifted_alphabet = alphabet[-shift:] + alphabet[:-shift]
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
 
-print(shifted_alphabet)
-# Output: ['x', 'y', 'z', 'a', 'b', 'c', ..., 'w']
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 

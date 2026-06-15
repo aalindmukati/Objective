442e24ff6eb78521cae7baab60c91eb1450c2fc6
Aalind Mukati <aalindmukati@gmail.com>
2026-06-15 12:52:28 +0530

    LONG AND LATI

--- ISS_Location/main.py
+++ ISS_Location/main.py
@@ -4,5 +4,8 @@ response = requests.get(url = 'http://api.open-notify.org/iss-now.json')
 response.raise_for_status()
 
 data = response.json()
-indent_data = json.dumps(data, indent=4)
-print(indent_data)
+long = data['iss_position']['longitude']
+lati = data['iss_position']['latitude']
+
+iss_pos = (lati,long)
+print(iss_pos)

206a65f23743f39d888db8233231661501b61dbb
Aalind Mukati <aalindmukati@gmail.com>
2026-06-15 12:47:58 +0530

    Indenting for better visibility

--- ISS_Location/main.py
+++ ISS_Location/main.py
@@ -3,5 +3,6 @@ import json
 response = requests.get(url = 'http://api.open-notify.org/iss-now.json')
 response.raise_for_status()
 
-data = response.json()['iss_position']
-print(data)
+data = response.json()
+indent_data = json.dumps(data, indent=4)
+print(indent_data)

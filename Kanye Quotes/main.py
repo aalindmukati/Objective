f95474a8ddd5b362cd9546a3e3f8f2d182d13f3b
Aalind Mukati <aalindmukati@gmail.com>
2026-06-15 16:51:46 +0530

    Final Changes in the kanye s#it xd

--- Kanye Quotes/main.py
+++ Kanye Quotes/main.py
@@ -1,8 +1,17 @@
 import requests
 import json
 
-quotes = requests.get(url='https://api.kanye.rest/')
-quotes.raise_for_status()
+def quoates():
+    quotes = requests.get(url='https://api.kanye.rest/')
+    quotes.raise_for_status()
 
-Kanye = quotes.json()
-print(Kanye)
+    Kanye = quotes.json()
+    print("---------------")
+    print(Kanye)
+
+a = input('Do you want a quote (yes) or (no) ').lower()
+
+if a == 'yes':
+    quoates()
+else:
+    print('Goodbye to the sun that shines for me no longer.')

01d1995b0e2f1a5dbd0c705d9ac31e868ea64f68
Aalind Mukati <aalindmukati@gmail.com>
2026-06-19 12:15:41 +0530

    99commits

--- List Comprehension/main.py
+++ List Comprehension/main.py
@@ -1,6 +1,7 @@
-# numbers = [1,2,3,4,5]
-# new_num = [n+1 for n in numbers]
-# # print(new_num)
+numbers = [1,2,3,4,5]
+new_num = [n+1 for n in numbers]
+# print(new_num)
 
-# range_list=[num* 2 for num in range(1,5)]
-# print(range_list)
+range_list=[num* 2 for num in range(1,5)]
+print(range_list)
+ 
